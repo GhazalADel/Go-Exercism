63340b6b851f2b867dcebb0c21ce7d682b389d64
GhazalADel <gashoftedel@aut.ac.ir>
2023-08-01 12:49:49 +0330

    Airport Robot

---
+++ AirportRobot/airport_robot.go
@@ -0,0 +1,34 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, greeter Greeter) string {
+	return greeter.Greet(name)
+}
+
+type Italian struct {
+}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("I can speak %s: Ciao %s!", i.LanguageName(), name)
+}
+
+type Portuguese struct {
+}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("I can speak %s: Olá %s!", p.LanguageName(), name)
+}

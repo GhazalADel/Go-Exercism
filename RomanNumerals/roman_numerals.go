5271b01a6a26f271d98c7937b261c33f5f89437a
GhazalADel <gashoftedel@aut.ac.ir>
2023-08-05 23:43:26 +0330

    Roman Numberals

---
+++ RomanNumerals/roman_numerals.go
@@ -0,0 +1,68 @@
+package romannumerals
+
+import (
+	"errors"
+)
+
+var mapping = map[int]string{
+	1:    "I",
+	2:    "II",
+	3:    "III",
+	4:    "IV",
+	5:    "V",
+	6:    "VI",
+	7:    "VII",
+	8:    "VIII",
+	9:    "IX",
+	10:   "X",
+	20:   "XX",
+	30:   "XXX",
+	40:   "XL",
+	50:   "L",
+	60:   "LX",
+	70:   "LXX",
+	80:   "LXXX",
+	90:   "XC",
+	100:  "C",
+	200:  "CC",
+	300:  "CCC",
+	400:  "CD",
+	500:  "D",
+	600:  "DC",
+	700:  "DCC",
+	800:  "DCCC",
+	900:  "CM",
+	1000: "M",
+	2000: "MM",
+	3000: "MMM",
+}
+
+func ToRomanNumeral(input int) (output string, err error) {
+	if input <= 0 {
+		return "", errors.New("")
+	} else if input > 3000 {
+		return "", errors.New("")
+	}
+
+	numerator := input
+	denominator := 1000
+	for numerator != 0 {
+		quotient, remainder := divmod(numerator, denominator)
+
+		output += convertToNumeral(quotient * denominator)
+
+		numerator = remainder
+		denominator = denominator / 10
+	}
+	return output, nil
+}
+
+func convertToNumeral(number int) string {
+	return mapping[number]
+}
+
+func divmod(numerator int, denominator int) (quotient, remainder int) {
+	quotient = numerator / denominator // integer division, decimals are truncated
+	remainder = numerator % denominator
+	return quotient, remainder
+}

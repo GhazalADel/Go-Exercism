ff18ef50dad055789db42fe68c094c67807b73c0
GhazalADel <gashoftedel@aut.ac.ir>
2023-08-06 10:43:38 +0330

    Resistor Color

---
+++ ResistorColor/resistor_color.go
@@ -0,0 +1,26 @@
+package resistorcolor
+
+import "strings"
+
+// Colors should return the list of all colors.
+func Colors() []string {
+	return []string{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	color = strings.ToLower(color)
+	colorCodes := map[string]int{
+		"black":  0,
+		"brown":  1,
+		"red":    2,
+		"orange": 3,
+		"yellow": 4,
+		"green":  5,
+		"blue":   6,
+		"violet": 7,
+		"grey":   8,
+		"white":  9,
+	}
+	return colorCodes[color]
+}

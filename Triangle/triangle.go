c236bbccc40986f354eaf8e2298368c532da5d44
GhazalADel <gashoftedel@aut.ac.ir>
2023-08-07 09:57:36 +0330

    Triangle

---
+++ Triangle/triangle.go
@@ -0,0 +1,33 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package triangle should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package triangle
+
+// Notice KindFromSides() returns this type. Pick a suitable data type.
+type Kind int
+
+const (
+	NaT Kind = iota // not a triangle
+	Equ             // equilateral
+	Iso             // isosceles
+	Sca             // scalene
+)
+
+// KindFromSides should have a comment documenting it.
+func KindFromSides(a, b, c float64) Kind {
+	if a <= 0 || b <= 0 || c <= 0 || a+b < c || a+c < b || b+c < a {
+		return NaT
+	}
+	if a == b && b == c {
+		return Equ
+	}
+	if a == b || a == c || b == c {
+		return Iso
+	}
+	if a != b && a != c && b != c {
+		return Sca
+	}
+	return NaT
+}
